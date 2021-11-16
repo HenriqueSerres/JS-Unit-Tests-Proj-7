99bcd5089d2eb8c20137f0a3897aaf75dc456066
Henrique Serres <henrique@gmail.com>
2021-11-16 17:41:30 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === '' || idade === 0) {
+  if (nome === ' ' || typeof idade !== 'number') {
     return undefined;
-  }  
+  } 
 return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`;
+#VQV!`;  
 };
-vqv('Tunico', 30);
+vqv();
 
 module.exports = vqv;
